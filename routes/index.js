0176571f1959035c8ed870138667a45f1de32d1c
Usama <musamadev@gmail.com>
2018-08-06 17:27:57 +0500

    Service label added

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
+// Service Label
+router.get('/', (req, res) => res.send("Video Microservice"));
+
 router.use('/video',    require('./videoRoutes'));
 router.use('/category', require('./categoryRoutes'));
 router.use('/program',  require('./programRoutes'));

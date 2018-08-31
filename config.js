4dd6ec34d20181c24eb1e3ce37da656e156ff09c
Usama <musamadev@gmail.com>
2018-08-31 17:18:10 +0500

    config updated

--- config.js
+++ config.js
@@ -3,12 +3,18 @@ const env = process.env.NODE_ENV || 'development';
 let config = {
     development: {
         port: '3000',
+        mongoDB: 'mongodb://localhost:27017/telenor',
+        transcodeServiceUrl: 'http://10.3.7.12:3011/v1/api/transcode'
     },
     staging: {
         port: '3000',
+        mongoDB: 'mongodb://localhost:27017/telenor',
+        transcodeServiceUrl: 'http://10.3.7.12:3011/v1/api/transcode'
     },
     production: {
         port: '3000',
+        mongoDB: 'mongodb://RootAdmin:password@10.3.7.101:27017/telenor?authSource=admin',
+        transcodeServiceUrl: 'http://10.3.7.12:3011/v1/api/transcode'
     }
 };
 
@@ -17,9 +23,5 @@ console.log("---", env);
 if (env === 'development') config = config.development;
 if (env === 'staging') config = config.staging;
 if (env === 'production') config = config.production;
-
-// Common configs
-config.mongoDB = 'mongodb://localhost:27017/telenor';
-config.transcodeServiceUrl = 'http://10.3.7.12:3011/v1/api/transcode';
 
 module.exports = config;

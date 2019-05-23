1027a90b206e07f68f4ecff9bb4c3f2e58a9a086
Usama <musamadev@gmail.com>
2019-05-23 15:31:18 +0500

    biodata removed

--- models/Anchor.js
+++ models/Anchor.js
@@ -7,7 +7,6 @@ const anchorSchema = new Schema({
     _id: ShortId,
     name: { type: String, trim: true },
     avatar: String,
-    biodata: String,
     weightage: Number,      // Can setup from 1-10 based on high to low, then sort them accordingly, default can be 0
     added_dtm: { type: Date, default: Date.now }
 })

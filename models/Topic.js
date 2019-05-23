6edd0f759c0e9f8e611bddd2d85696965df220ea
Usama <musamadev@gmail.com>
2019-05-23 15:30:55 +0500

    description removed

--- models/Topic.js
+++ models/Topic.js
@@ -6,7 +6,6 @@ const {Schema} = mongoose;
 const topicSchema = new Schema({
     _id: ShortId,
     name: { type: String, trim: true },
-    description: String,
     weightage: Number,      // From 1-10 based on high to low priority, 0=lowest and 10=highest
     added_dtm: { type: Date, default: Date.now }
 });

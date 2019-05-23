c9ef60755fd2e3f561fbb813e241e4a0a9db600c
Usama <musamadev@gmail.com>
2019-05-23 15:31:45 +0500

    description removed

--- models/Program.js
+++ models/Program.js
@@ -6,7 +6,6 @@ const {Schema} = mongoose;
 const programSchema = new Schema({
     _id: ShortId,   //Generating shortid instead of uuid
     name: { type: String, trim: true },
-    description: String,
     weightage: Number,
     added_dtm: { type: Date, default: Date.now }
 });

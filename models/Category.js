c2c07e5d81d9e5a79497cdfbcdfaaa409b5a3732
Farhan Ali <farhan.ali@dmdmax.com>
2020-08-26 13:47:09 +0500

    models added

--- models/Category.js
+++ models/Category.js
@@ -1,24 +1,13 @@
 const mongoose = require('mongoose');
 const ShortId = require('mongoose-shortid-nodeps');
-const shortid = require('shortid');
 const {Schema} = mongoose;
 
 
 // Sub category can be removed as they were removed from frontend in new design (Just keeping in case they change their mind)
-const subcatSchema = new Schema({ 
-    _id: {
-        type: String,
-        'default': shortid.generate     //Because the mongoose shortid does not work on nested schemas
-    },
-    name: { type: String, trim: true },
-    description: String,
-    added_dtm: { type: Date, default: Date.now }
-});
-
 const categorySchema = new Schema({
     _id: ShortId,
     name: { type: String, trim: true },
-    sub_categories: [subcatSchema],
+    sub_categories: Array,
     added_dtm: { type: Date, default: Date.now }
 });
 

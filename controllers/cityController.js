6957873da7d5428713406a2f6c7d50f9acc90e54
Taha <taha@dmdmax.com.pk>
2021-03-08 12:16:01 +0500

    city field

--- controllers/cityController.js
+++ controllers/cityController.js
@@ -4,15 +4,15 @@ const City = mongoose.model('City');
 // READ
 exports.get = async (req, res) => {
 
-	let { _id, name, province } = req.query;
+	let { _id, city, province } = req.query;
 	const query = {};
 
 	if (_id) query._id = _id;
-	if (name) query.name = name;
+	if (city) query.city = city;
 	if (province) query.province = province;		// Conversion of string to Boolean
 
 	let result;
-	if (_id || name)
+	if (_id || city)
 		result = await City.findOne(query);
 	else
 		result = await City.find(query).sort({city: 1});
